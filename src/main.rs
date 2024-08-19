5cd3befaa06c36f123cdc5150a8332e1c264fa8a
eto8201 <namjun88@gmail.com>
2024-08-19 22:10:33 +0900

    함수 연습하기1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 fn main() {
+
+    practice_variable();
+}
+
+
+// rust는 snake_case를 더 선호하는 듯?
+fn practice_variable() {
     println!("변수 연습하기");
     let name = "홍길동";
+    // let으로 변수 표현이 가능합니다.
+    let age = 40;
+    // 숫자 역시 가능합니다.
 
     println!("당신의 이름은 => {} 입니다.", name);
+    println!("당신의 나이는 => {} 입니다.", age);
+
+    let your_name = "Mike";
+    let my_name = "Namjun";
+
+    println!("Your name is {}, and my name is {}", your_name, my_name);
+    // 이렇게 연속으로 가능합니다.
 
 }
